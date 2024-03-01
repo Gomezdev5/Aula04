c91363ba27561411ab7b8bd4be515176372bb788
Gomezdev5 <150376296+Gomezdev5@users.noreply.github.com>
2024-03-01 19:56:29 +0000

    versão 5

---
+++ questao26.py
@@ -0,0 +1,7 @@
+"""Escreva um programa que peça ao usuário para digitar uma frase e imprima o número de caracteres na frase."""
+
+frase = input("Digite uma frase")
+
+letras = len(frase)
+
+print(letras)
---
+++ questao27.py
@@ -0,0 +1,2 @@
+""" Escreva um programa que solicite ao usuário para digitar seu nome em letras minúsculas e, em seguida, imprima o nome com a primeira letra em maiúscula. """
+
