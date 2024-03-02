fb504eade91af09e81ee2a4723315c0a0f4ee6a7
Gomezdev5 <150376296+Gomezdev5@users.noreply.github.com>
2024-03-02 01:43:55 +0000

    Versão 07

--- questao27.py
+++ questao27.py
@@ -1,10 +1,8 @@
 """ Escreva um programa que solicite ao usuário para digitar seu nome em letras minúsculas e, 
 em seguida, imprima o nome com a primeira letra em maiúscula. """
 
-import re
-
 nome = input("Digite seu nome em letras minúsculas")
 
 maiuscula = nome.upper()
 
-print(maiscula)
+print(maiuscula)
--- questao28.py
+++ questao28.py
@@ -1,3 +1,6 @@
 """Escreva um programa que solicite ao usuário para digitar uma frase e, em seguida, 
 divida a frase em palavras individuais e as imprima em linhas separadas."""
 
+frase = input("Digite uma frase: ")
+
+print("Palavras na frase:", *frase.split(), sep='\n')
